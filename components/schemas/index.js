e6dd422f8406c687a9dd1afa1c9a9bb1664614ce
sdbarlow <sethdbarlow@gmail.com>
2023-07-04 08:39:53 -0700

    small changes

--- components/schemas/index.js
+++ components/schemas/index.js
@@ -1,6 +1,24 @@
 import * as yup from "yup";
 
 export const basicSchema = yup.object().shape({
-    email: yup.string().email("please enter a valid email").required("Required"),
+    firstName: yup.string().required("First Name is required"),
+    lastName: yup.string().required("Last Name is required"),
+    middleInitial: yup.string().required("Middle Initial is required"),
+    date: yup.string().required("Date is required"),
+    address: yup.string().required("Address is required"),
+    aptNum: yup.string().required("Apartment Number is required"),
+    city: yup.string().required("City is required"),
+    state: yup.string().required("State is required"),
+    zipCode: yup.string().required("Zip Code is required"),
+    phone: yup.string().required("Phone number is required"),
+    dateAvailable: yup.string().required("Date Available is required"),
+    socialSecurity: yup.string().required("Social Security Number is required"),
+    desiredPay: yup.string().required("Desired Pay is required"),
+    position: yup.string().required("Position is required"),
+    legalEligibility: yup.string().required("Legal Eligibility is required"),
+    felonyBool: yup.string().required("Felony Status is required"),
+    felonyExplanation: yup.string().required("Felony Explanation is required"),
+    email: yup.string().email("please enter a valid email").required("Email is Required"),
+
 
 })
--- pages/application.js
+++ pages/application.js
@@ -20,7 +20,7 @@ function Application() {
             middleInitial: "",
             date: "",
             address: "",
-            aptNUm: "",
+            aptNum: "",
             city: "",
             state: "",
             zipCode: "",
@@ -38,10 +38,12 @@ function Application() {
         onSubmit,
     })
 
-    console.log(errors)
+    const hasErrors = Object.values(errors).some((error) => error);
+
+    console.log(hasErrors);
   return (
     <div className='bg-blue-500 h-fit flex justify-center items-center pt-56'>
-        <div className='bg-white h-[1000px] w-3/4 rounded-3xl mb-20 relative shadow-[inset_0_-1px_0px_12px_rgb(106,188,239)]'>
+        <div className={hasErrors ? 'bg-white h-[1150px] w-3/4 rounded-3xl mb-20 relative shadow-[inset_0_-1px_0px_12px_rgb(106,188,239)]' : 'bg-white h-[1000px] w-3/4 rounded-3xl mb-20 relative shadow-[inset_0_-1px_0px_12px_rgb(106,188,239)]'}>
             <div className='bg-blue-500 border-b-8 border-r-8 border-custom rounded-br-3xl h-1/5 w-1/4 absolute top-0 left-0'>
                 <div className='w-full h-full bg-gradient-radial from-white via-blue-500 via-70% flex items-center pt-16 justify-center'>
                     <Image src={FullGrowingPlace} height={900} width={360} className='select-none'></Image>
@@ -54,7 +56,7 @@ function Application() {
             <div className='bg-white ml-[29%] h-[13%] w-2/3 flex flex-col items-center justify-end shadow-[inset_8px_11px_0px_0px_rgb(106,188,239)]'>
                 <h1 className='text-pengblue text-6xl font-sniglet z-10 select-none'>Employment Application</h1>
             </div>
-            <div className='w-[90%] m-auto mb-8 mt-24 h-[500px] bg-white relative z-10'>
+            <div className={hasErrors ? "w-[90%] m-auto mb-8 mt-28 h-[500px] bg-white relative z-10" : "w-[90%] m-auto mb-8 mt-24 h-[500px] bg-white relative z-10"}>
                     <form onSubmit={handleSubmit} autoComplete='off'>
                         <div className='w-20 h-28 inline-flex'>
                             <div className='rounded-full h-[50%] w-[70%] flex items-center border-2 border-gray-500 justify-center bg-pengblue z-10'>
@@ -67,37 +69,44 @@ function Application() {
                             <div className='flex flex-col justify-between'>
                                 <label className='text-black font-semibold select-none' htmlFor='firstName'>First Name:</label>
                                 <br></br>
-                                <input className={errors.email && touched.email ? "border-2 border-red-600 p-2 rounded-lg grid-col-1 row-span-1" : "border-2 border-gray-400 p-2 rounded-lg grid-col-1 row-span-1"} value={values.firstName} onChange={handleChange} onBlur={handleBlur} id="firstName" type="text" placeholder='John'></input>
+                                <input className={errors.firstName && touched.firstName ? "border-2 border-red-600 p-2 rounded-lg grid-col-1 row-span-1" : "border-2 border-gray-400 p-2 rounded-lg grid-col-1 row-span-1"} value={values.firstName} onChange={handleChange} onBlur={handleBlur} id="firstName" type="text" placeholder='John'></input>
+                                {errors.firstName && touched.firstName && <p className='text-red-500'>* {errors.firstName}</p> }
                             </div>
                             <div className='flex flex-col justify-between'>
                                 <label className='text-black font-semibold select-none' htmlFor='lastName'>Last Name:</label>
                                 <br></br>
-                                <input className={errors.email && touched.email ? "border-2 border-red-600 p-2 rounded-lg grid-col-1 row-span-1" : "border-2 border-gray-400 p-2 rounded-lg grid-col-1 row-span-1"} value={values.lastName} onChange={handleChange} onBlur={handleBlur} id="lastName" type="text" placeholder='Doe'></input>
+                                <input className={errors.lastName && touched.lastName ? "border-2 border-red-600 p-2 rounded-lg grid-col-1 row-span-1" : "border-2 border-gray-400 p-2 rounded-lg grid-col-1 row-span-1"} value={values.lastName} onChange={handleChange} onBlur={handleBlur} id="lastName" type="text" placeholder='Doe'></input>
+                                {errors.lastName && touched.lastName && <p className='text-red-500'>* {errors.lastName}</p> }
                             </div>
                             <div className='flex flex-col justify-between'>
-                                <label className='text-black font-semibold select-none' htmlFor='firstName'>Middel Initial:</label>
+                                <label className='text-black font-semibold select-none' htmlFor='middleInitial'>Middel Initial:</label>
                                 <br></br>
-                                <input className={errors.email && touched.email ? "border-2 border-red-600 p-2 rounded-lg grid-col-1 row-span-1" : "border-2 border-gray-400 p-2 rounded-lg grid-col-1 row-span-1"} value={values.middleInitial} onChange={handleChange} onBlur={handleBlur} id="firstName" type="text" placeholder='B'></input>
+                                <input className={errors.middleInitial && touched.middleInitial ? "border-2 border-red-600 p-2 rounded-lg grid-col-1 row-span-1" : "border-2 border-gray-400 p-2 rounded-lg grid-col-1 row-span-1"} value={values.middleInitial} onChange={handleChange} onBlur={handleBlur} id="middleInitial" type="text" placeholder='B'></input>
+                                {errors.middleInitial && touched.middleInitial && <p className='text-red-500'>* {errors.middleInitial}</p> }
                             </div>
                             <div className='flex flex-col justify-between'>
-                                <label className='text-black font-semibold select-none' htmlFor='firstName'>Address:</label>
+                                <label className='text-black font-semibold select-none' htmlFor='address'>Address:</label>
                                 <br></br>
-                                <input className={errors.email && touched.email ? "border-2 border-red-600 p-2 rounded-lg grid-col-1 row-span-1" : "border-2 border-gray-400 p-2 rounded-lg grid-col-1 row-span-1"} value={values.address} onChange={handleChange} onBlur={handleBlur} id="firstName" type="text" placeholder='1234 example st'></input>
+                                <input className={errors.address && touched.address ? "border-2 border-red-600 p-2 rounded-lg grid-col-1 row-span-1" : "border-2 border-gray-400 p-2 rounded-lg grid-col-1 row-span-1"} value={values.address} onChange={handleChange} onBlur={handleBlur} id="address" type="text" placeholder='1234 example st'></input>
+                                {errors.address && touched.address && <p className='text-red-500'>* {errors.address}</p> }
                             </div>
                             <div className='flex flex-col justify-between'>
-                                <label className='text-black font-semibold select-none' htmlFor='firstName'>Apartment/Unit #:</label>
+                                <label className='text-black font-semibold select-none' htmlFor='aptNum'>Apartment/Unit #:</label>
                                 <br></br>
-                                <input className={errors.email && touched.email ? "border-2 border-red-600 p-2 rounded-lg grid-col-1 row-span-1" : "border-2 border-gray-400 p-2 rounded-lg grid-col-1 row-span-1"} value={values.aptNUm} onChange={handleChange} onBlur={handleBlur} id="firstName" type="text" placeholder='213'></input>
+                                <input className={errors.aptNum && touched.aptNum ? "border-2 border-red-600 p-2 rounded-lg grid-col-1 row-span-1" : "border-2 border-gray-400 p-2 rounded-lg grid-col-1 row-span-1"} value={values.aptNUm} onChange={handleChange} onBlur={handleBlur} id="aptNum" type="text" placeholder='213'></input>
+                                {errors.aptNum && touched.aptNum && <p className='text-red-500'>* {errors.aptNum}</p> }
                             </div>
                             <div className='flex flex-col justify-between'>
-                                <label className='text-black font-semibold select-none' htmlFor='firstName'>City:</label>
+                                <label className='text-black font-semibold select-none' htmlFor='city'>City:</label>
                                 <br></br>
-                                <input className={errors.email && touched.email ? "border-2 border-red-600 p-2 rounded-lg grid-col-1 row-span-1" : "border-2 border-gray-400 p-2 rounded-lg grid-col-1 row-span-1"} value={values.city} onChange={handleChange} onBlur={handleBlur} id="firstName" type="text" placeholder='Your City'></input>
+                                <input className={errors.city && touched.city ? "border-2 border-red-600 p-2 rounded-lg grid-col-1 row-span-1" : "border-2 border-gray-400 p-2 rounded-lg grid-col-1 row-span-1"} value={values.city} onChange={handleChange} onBlur={handleBlur} id="city" type="text" placeholder='Your City'></input>
+                                {errors.city && touched.city && <p className='text-red-500'>* {errors.city}</p> }
                             </div>
                             <div className='flex flex-col justify-between'>
                                 <label className='text-black font-semibold select-none' htmlFor='phone'>Phone:</label>
                                 <br></br>
-                                <input className={errors.email && touched.email ? "border-2 border-red-600 p-2 rounded-lg grid-col-1 row-span-1" : "border-2 border-gray-400 p-2 rounded-lg grid-col-1 row-span-1"} value={values.phone} onChange={handleChange} onBlur={handleBlur} id="phone" type="text" placeholder='e.g. +1-416-555-1212'></input>
+                                <input className={errors.phone && touched.phone ? "border-2 border-red-600 p-2 rounded-lg grid-col-1 row-span-1" : "border-2 border-gray-400 p-2 rounded-lg grid-col-1 row-span-1"} value={values.phone} onChange={handleChange} onBlur={handleBlur} id="phone" type="text" placeholder='e.g. +1-416-555-1212'></input>
+                                {errors.phone && touched.phone && <p className='text-red-500'>* {errors.phone}</p> }
                             </div>
                             <div className='flex flex-col justify-between'>
                                 <label className='text-black font-semibold select-none' htmlFor='email'>Email:</label>
@@ -108,29 +117,34 @@ function Application() {
                             <div className='flex flex-col justify-between'>
                                 <label className='text-black font-semibold select-none' htmlFor='date'>Date Available:</label>
                                 <br></br>
-                                <input className={errors.email && touched.email ? "border-2 border-red-600 p-2 rounded-lg grid-col-1 row-span-1" : "border-2 border-gray-400 p-2 rounded-lg grid-col-1 row-span-1"} value={values.date} onChange={handleChange} onBlur={handleBlur}  id="date" type="date"></input>
+                                <input className={errors.dateAvailable && touched.dateAvailable ? "border-2 border-red-600 p-2 rounded-lg grid-col-1 row-span-1" : "border-2 border-gray-400 p-2 rounded-lg grid-col-1 row-span-1"} value={values.date} onChange={handleChange} onBlur={handleBlur}  id="date" type="date"></input>
+                                {errors.dateAvailable && touched.dateAvailable && <p className='text-red-500'>* {errors.dateAvailable}</p> }
                             </div>
                             <div className='flex flex-col justify-between'>
-                                <label className='text-black font-semibold select-none' htmlFor='socialsecurity'>Social Security No.</label>
+                                <label className='text-black font-semibold select-none' htmlFor='socialSecurity'>Social Security No.</label>
                                 <br></br>
-                                <input className={errors.email && touched.email ? "border-2 border-red-600 p-2 rounded-lg grid-col-1 row-span-1" : "border-2 border-gray-400 p-2 rounded-lg grid-col-1 row-span-1"} value={values.socialSecurity} onChange={handleChange} onBlur={handleBlur} id="socialsecurity" type="text" placeholder='SSN'></input>
+                                <input className={errors.socialSecurity && touched.socialSecurity ? "border-2 border-red-600 p-2 rounded-lg grid-col-1 row-span-1" : "border-2 border-gray-400 p-2 rounded-lg grid-col-1 row-span-1"} value={values.socialSecurity} onChange={handleChange} onBlur={handleBlur} id="socialSecurity" type="text" placeholder='SSN'></input>
+                                {errors.socialSecurity && touched.socialSecurity && <p className='text-red-500'>* {errors.socialSecurity}</p> }
                             </div>
                             <div className='flex flex-col justify-between'>
-                                <label className='text-black font-semibold select-none' htmlFor='desiredpay'>Desired Pay:</label>
+                                <label className='text-black font-semibold select-none' htmlFor='desiredPay'>Desired Pay:</label>
                                 <br></br>
-                                <input className={errors.email && touched.email ? "border-2 border-red-600 p-2 rounded-lg grid-col-1 row-span-1" : "border-2 border-gray-400 p-2 rounded-lg grid-col-1 row-span-1"} value={values.desiredPay} onChange={handleChange} onBlur={handleBlur} id="firstName" type="text" placeholder='Desired Pay'></input>
+                                <input className={errors.desiredPay && touched.desiredPay ? "border-2 border-red-600 p-2 rounded-lg grid-col-1 row-span-1" : "border-2 border-gray-400 p-2 rounded-lg grid-col-1 row-span-1"} value={values.desiredPay} onChange={handleChange} onBlur={handleBlur} id="desiredPay" type="text" placeholder='Desired Pay'></input>
+                                {errors.desiredPay && touched.desiredPay && <p className='text-red-500'>* {errors.desiredPay}</p> }
                             </div>
                             <div className='flex flex-col justify-between'>
                                 <label className='text-black font-semibold select-none' htmlFor='position'>Position Applied For:</label>
                                 <br></br>
-                                <select className={errors.email && touched.email ? "border-2 border-red-600 p-2 rounded-lg grid-col-1 row-span-1" : "border-2 border-gray-400 p-2 rounded-lg grid-col-1 row-span-1"} value={values.position} onChange={handleChange} onBlur={handleBlur} id="position">
+                                <select className={errors.position && touched.position ? "border-2 border-red-600 p-2 rounded-lg grid-col-1 row-span-1" : "border-2 border-gray-400 p-2 rounded-lg grid-col-1 row-span-1"} value={values.position} onChange={handleChange} onBlur={handleBlur} id="position">
                                     <option>CareTaker</option>
                                     <option>CareTaker 2</option>
                                     <option>CareTaker 3</option>
                                 </select>
+                                {errors.position && touched.position && <p className='text-red-500'>* {errors.position}</p> }
                             </div>
                             <div className='col-span-2 flex justify-between items-center'>
-                                <h1 className='text-black font-semibold inline' htmlFor='position'>Are you legally eligible for employment in the US?</h1>
+                                <h1 className='text-black font-semibold inline'>Are you legally eligible for employment in the US?</h1>
+                                {errors.legalEligibility && touched.legalEligibility && <p className='text-red-500'>* {errors.legalEligibility}</p> }
                                 <div className='flex flex-col'>
                                     <label htmlfor='yes'>Yes</label>
                                     <input id='yes' type="checkbox"></input>
@@ -141,7 +155,8 @@ function Application() {
                                 </div>
                             </div>
                             <div className='col-span-2 flex justify-between items-center'>
-                                <h1 className='text-black font-semibold inline' htmlFor='position'>Have you ever been convicted of a felony?</h1>
+                                <h1 className='text-black font-semibold inline'>Have you ever been convicted of a felony?</h1>
+                                {errors.felonyBool && touched.felonyBool && <p className='text-red-500'>* {errors.felonyBool}</p> }
                                 <div className='flex flex-col'>
                                     <label htmlfor='yes'>Yes</label>
                                     <input id='yes' type="checkbox"></input>
